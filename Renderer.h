fb69c0a8a596ad752bdd56bc33f588d3b6723e44
Artem Yudin <arrannis1800@gmail.com>
2024-06-10 14:52:29 +0400

    create separate file for color struct

--- Renderer.h
+++ Renderer.h
@@ -9,29 +9,6 @@
 #include "Structs/AVideoParams.h"
 #include "Structs/LevelSettings.h"
 
-struct ColorRGBA
-{
-	ColorRGBA() = default;
-
-	ColorRGBA(uint32_t color)
-	{
-		r = (color >> (8*3)) & 0xff;
-		g = (color >> (8*2)) & 0xff;
-		b = (color >> (8*1)) & 0xff;
-		a = (color >> (8*0)) & 0xff;
-	}
-
-	uint32_t toUInt32()
-	{
-		return (r << 24) | (g << 16) | (b << 8) | a;
-	};
-
-	uint8_t r;
-	uint8_t g;
-	uint8_t b;
-	uint8_t a;
-};
-
 class AWindow
 {
 public:
