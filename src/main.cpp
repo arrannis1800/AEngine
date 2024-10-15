4d6f79879f91ff1dea933a8ccd28bc7d8dd0b0f8
Artem Yudin <arrannis1800@gmail.com>
2024-10-15 12:58:37 +0400

    add main

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+#include "Engine.h"
+#include "Game.h"
+#include "Config.h"
+#include "Structs/AGState.h"
+#include "Resources/ResourceManager.h"
+
+
+int main(int argc, char* args[])
+{
+	(void)argc;
+	std::string m_path(args[0]);
+	size_t position = m_path.find_last_of('/\\');
+	gState.path = m_path.substr(0, position);
+	AGame game;
+	AEngine engine(&game);
+	AResourceManager resourceManager;
+	gState.ResourceManager = &resourceManager;
+	gState.engine = &engine;
+	gState.GetEngine()->Start();
+
+	return 0;
+}
